eed7a6d856f92af84c01405ae79ad868ad32dddf
Ciaran Hurst <hursting@gmail.com>
2021-06-12 12:52:16 +0100

    Added header component

---
+++ src/components/header.js
@@ -0,0 +1,52 @@
+import React from 'react'
+
+
+const LoggedOutView = props => {
+    if (!props.currentUser) {
+      return (
+        <ul className="nav navbar-nav pull-xs-right">
+  
+          <li className="nav-item">
+            <Link to="/" className="nav-link">
+              Home
+            </Link>
+          </li>
+  
+          <li className="nav-item">
+            <Link to="/login" className="nav-link">
+              Sign in
+            </Link>
+          </li>
+  
+          <li className="nav-item">
+            <Link to="/register" className="nav-link">
+              Sign up
+            </Link>
+          </li>
+  
+        </ul>
+      );
+    }
+    return null;
+  };
+
+  class Header extends  React.Component{
+    render() {
+        return (
+          <nav className="navbar navbar-light">
+            <div className="container">
+    
+              <Link to="/" className="navbar-brand">
+                {this.props.appName.toLowerCase()}
+              </Link>
+    
+              <LoggedOutView currentUser={this.props.currentUser} />
+    
+              <LoggedInView currentUser={this.props.currentUser} />
+            </div>
+          </nav>
+        );
+      }
+  }
+
+  export default Header
